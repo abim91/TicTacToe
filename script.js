8bd3a3690bdcc4c94bf7d6ef5c16ac351b89cf85
Abim Shrestha <53064345+abim91@users.noreply.github.com>
2024-07-16 20:28:41 -0400

    Fix game control Error

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let gameBoard = [{value: .0, available: true}, {value: .1,available: true}, {value: .2, available: true},
-             {value: .3,  available: true},{value: .4, available: true},{value: .5, available: true},
-             {value: .6,  available: true},{value: .7, available: true},{value: .8, available: true}];
+let gameBoard = [{value: 0, available: true}, {value: 1,available: true}, {value: 2, available: true},
+             {value: 3,  available: true},{value: 4, available: true},{value: 5, available: true},
+             {value: 6,  available: true},{value: 7, available: true},{value: 8, available: true}];
+
+/*let gameBoard = [{value: "", available: true}, {value: "",available: true}, {value: "", available: true},
+                {value: "",  available: true},{value: "", available: true},{value: "", available: true},
+                {value: "",  available: true},{value: "", available: true},{value: "", available: true}];*/
 
 function displayGrid(){
     let output = "";
@@ -50,15 +54,34 @@ function detectWinner(){
     for(let i = 0; i < 9; i+=3){
         
         if(gameBoard[i].value === gameBoard[i+1].value && gameBoard[i+1].value === gameBoard[i+2].value){
-            
+            console.log("horizontal win");
             return true;
         }
     }
     //check Vertical for winners, check if all the horizontals are the same
-
+    for(let i = 0; i < 3; i++){
+        
+        if(gameBoard[i].value === gameBoard[i+3].value && gameBoard[i+3].value === gameBoard[i+6].value){
+            console.log("vertical win");
+            return true;
+        }
+    }
     //check diagonal for winners, check if all the horizontals are the same
-
+    for(let i = 0; i < 1; i++){
+        
+        if(gameBoard[i].value === gameBoard[i+4].value && gameBoard[i+4].value === gameBoard[i+8].value){
+            console.log("diagonal win");
+            return true;
+        }
+    }
     //check anti-diagonal for winners, check if all the horizontals are the same
+    for(let i = 2; i < 3; i++){
+        
+        if(gameBoard[i].value === gameBoard[i*2].value && gameBoard[i*2].value === gameBoard[i*3].value){
+            console.log("anti-diagonal win");
+            return true;
+        }
+    }
     return false;
 }
 /*
@@ -79,9 +102,9 @@ while (true){
         break;
     }
 
-}
+}*/
  
-*/
+
 
 
 /*for(let i = 0; i < eachSlot.length ; i++){
@@ -97,52 +120,78 @@ let eachSlot = ["zero","one","two","three","four","five","six","seven","eight"];
         const slot = document.getElementById(`slot-${eachSlot[i]}`);
         slot.innerText = gameBoard[i].value; 
     }
-};
+};*/
 
-updateGrid();*/
-
+//updateGrid();
+/*function end(){
+    for(let i = 0; i < eachSlot.length ; i++){
+        const clickedCell = document.getElementById(`slot-${eachSlot[i]}`);
+        clickedCell.removeEventListener("click",eventHandler());
+    }
+}*/
+function eventHandler(i, clickedCell){
+    if(gameBoard[i].available){ 
+        currentlySelected = clickedCell;
+        currentlySelected.innerText = currentPlayer.type;
+        
+        gameBoard[i].available = false;
+        gameBoard[i].value = currentPlayer.type; //updates the gameboard array
+      
+        if(detectWinner()){
+            console.log(gameBoard);
+            console.log(currentPlayer.type, " is the winner. yessir");
+            end();
+            
+        }
+        else{
+            currentPlayer = (currentPlayer === p1)? p2: p1;
+            play(currentPlayer);
+        }
+    }
+    
+}
 function play(cP){
     currentPlayer = cP;
     let currentlySelected;
+    
+    
     for(let i = 0; i < eachSlot.length ; i++){
+        
         const clickedCell = document.getElementById(`slot-${eachSlot[i]}`);
         clickedCell.addEventListener("click",(e)=>{
+            document.getElementById("playerTracker").hidden = true;
+            if(gameBoard[i].available){ 
             currentlySelected = clickedCell;
             currentlySelected.innerText = currentPlayer.type;
+            
+            gameBoard[i].available = false;
             gameBoard[i].value = currentPlayer.type; //updates the gameboard array
+          
+            if(detectWinner()){
+                console.log(gameBoard);
+                console.log(currentPlayer.type, " is the winner. yessir");
+                document.getElementById("winnerDisplay").innerText = `PLAYER ${currentPlayer.type} IS THE WINNER`
+                
+                
+            }
+            else{
+                currentPlayer = (currentPlayer === p1)? p2: p1;
+                play(currentPlayer);
+            }
+        }
+        
         });
+        
     }
     
+    
+    
 }
-//play();
+
 
 function runner(){
-    
-    for(let i = 0; i < 9; i++){
-        console.log("inhere");
-        
-        if(detectWinner()){
-        console.log("Player", p1.type ,"is the winner!!");
-        break;
-        }
-        else{
-            play(p1);
-        }
-    /*    else{
-            play(p2);
-        }*/
-        
-        play(p2);
-        if(detectWinner()){
-            console.log("Player", p1.type ,"is the winner!!");
-            break;
-        }
-   /*     else{
-            play(p1);
-        }*/
-        
-
-
+    play(p1);
+    return;
+   
     }
-}
 runner();
